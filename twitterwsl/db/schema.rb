aa913ed98d9b8fa203bae742bc16aedb1a003785
AlexUkPC <alexrogna@yahoo.com>
2021-03-31 13:43:49 +0100

    Scaffold Tweeet

--- twitterwsl/db/schema.rb
+++ twitterwsl/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_124110) do
+ActiveRecord::Schema.define(version: 2021_03_31_124312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweeets", force: :cascade do |t|
+    t.text "tweeet"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
